f7f84eb8c73621962f03189cb1ac95dfd5abaa59
John Kelly <j.kelly@cabem.com>
2022-12-30 15:20:58 -0500

    hoping route works

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import "../styles/globals.css";
 import * as React from "react";
-import Layout from "../components/layout.js";
+import Layout from "../components/layout";
 
 export default function App({ Component, pageProps }) {
   return (
