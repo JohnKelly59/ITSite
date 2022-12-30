affb64d4f91405ab9b104d55313d37e6de0bbeb1
John Kelly <j.kelly@cabem.com>
2022-12-30 12:58:00 -0500

    form email system cokplete

--- components/contactus/AppointmentForm.js
+++ components/contactus/AppointmentForm.js
@@ -7,37 +7,67 @@ import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Zoom from "@mui/material/Zoom";
-import { MuiTelInput } from "mui-tel-input";
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
-import { DesktopDatePicker } from "@mui/x-date-pickers/DesktopDatePicker";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+
 import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select from "@mui/material/Select";
-
-import dayjs from "dayjs";
+import { MuiTelInput } from "mui-tel-input";
+import emailjs from "@emailjs/browser";
+import { SuccessAlert, FailureAlert } from "../Alerts";
 
 export default function AppointmentForm(props) {
+  const [successOpen, setSuccessOpen] = React.useState(false);
+  const [failureOpen, setFailureOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSuccessOpen(false);
+    setFailureOpen(false);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.number.value);
+    let template = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      number: e.target.number.value,
+      message: e.target.message.value,
+      serviceRequest: serviceValue,
+      insurance: insuranceValue,
+    };
+
+    emailjs
+      .send(
+        "service_ulbsev4",
+        "template_6dskuie",
+        template,
+        "JjtNNxBts-8mfP1NA"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccessOpen(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setFailureOpen(true);
+        }
+      );
   };
+
   const [phone, setPhone] = React.useState("");
   const handlePhoneChange = (newValue) => {
     console.log(newValue);
     setPhone(newValue);
   };
 
-  const [dateValue, setDateValue] = React.useState(dayjs());
+  const [serviceValue, setServiceValue] = React.useState("");
 
-  const handleDateChange = (newValue) => {
-    setDateValue(newValue);
-  };
-  const [timeValue, setTimeValue] = React.useState("");
-
-  const handleTimeChange = (newValue) => {
-    setTimeValue(newValue.target.value);
+  const handleServiceChange = (newValue) => {
+    setServiceValue(newValue.target.value);
   };
 
   const [insuranceValue, setInsuranceValue] = React.useState("");
@@ -48,127 +78,138 @@ export default function AppointmentForm(props) {
 
   return (
     <>
+      <SuccessAlert open={successOpen} handleClose={handleClose} />
+      <FailureAlert open={failureOpen} handleClose={handleClose} />
       <Zoom in={true} style={{ transitionDelay: "250ms" }}>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 6, width: "55ch" },
-          }}
-          noValidate
-          autoComplete="off"
-          md={3}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <Paper
-            sx={{ m: 2, backgroundColor: "#9dc53f", borderRadius: "16px" }}
-            elevation={3}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <Box
+            sx={{
+              "& > :not(style)": { m: 6, width: "55ch" },
+            }}
+            noValidate
+            autoComplete="off"
+            md={3}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
+            onSubmit={(e) => handleSubmit(e)}
           >
-            <Stack
-              sx={{ m: 5 }}
-              direction={"column"}
-              spacing={2}
-              textAlign="center"
+            <Paper
+              sx={{ m: 2, backgroundColor: "#9dc53f", borderRadius: "16px" }}
+              elevation={3}
             >
-              <Typography variant={"h5"}>Request an Appointment</Typography>
-              <TextField required id="name" label="Name" variant="outlined" />
-              <TextField
-                type="email"
-                required
-                id="email"
-                label="Email"
-                variant="outlined"
-              />
-              <MuiTelInput
-                value={phone}
-                onChange={handlePhoneChange}
-                required
-                id="number"
-                label="Number"
-                variant="outlined"
-              />
-              <FormControl required fullWidth>
-                <InputLabel id="appointmentTime">Service Request</InputLabel>
+              <Stack
+                sx={{ m: 5 }}
+                direction={"column"}
+                spacing={2}
+                textAlign="center"
+              >
+                <Typography variant={"h5"}>Request an Appointment</Typography>
+                <TextField required id="name" label="Name" variant="outlined" />
+                <TextField
+                  type="email"
+                  required
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                />
+                <MuiTelInput
+                  value={phone}
+                  onChange={handlePhoneChange}
+                  required
+                  id="number"
+                  label="Number"
+                  variant="outlined"
+                />
+                <FormControl id="serviceRequest" required fullWidth>
+                  <InputLabel id="serviceRequest">Service Request</InputLabel>
+                  <Select
+                    required
+                    labelId="serviceRequest"
+                    id="serviceRequest"
+                    value={serviceValue}
+                    label="Service Request"
+                    onChange={handleServiceChange}
+                  >
+                    <MenuItem value={"Physical Therapy"}>
+                      Physical Therapy
+                    </MenuItem>
+                    <MenuItem value={"Occupational Therapy"}>
+                      Occputaional Therapy
+                    </MenuItem>
+                    <MenuItem value={"Speech Therapy"}>Speech Therapy</MenuItem>
+                    <MenuItem value={"Aquatic Therapy"}>
+                      Aquatic Therapy
+                    </MenuItem>
+                    <MenuItem value={"ABA Therapy"}>ABA Therapy</MenuItem>
+                    <MenuItem value={"Massage Therapy"}>
+                      Massage Therapy
+                    </MenuItem>
+                  </Select>
+                </FormControl>
+                <FormControl id="insurance" required fullWidth>
+                  <InputLabel id="insurance">Insurance</InputLabel>
 
-                <Select
-                  labelId="appointmentTime"
-                  id="appointmentTime"
-                  value={timeValue}
-                  label="Appointment Time"
-                  onChange={handleTimeChange}
-                >
-                  <MenuItem value={"Physical Therapy"}>
-                    Physical Therapy
-                  </MenuItem>
-                  <MenuItem value={"Occupational Therapy"}>
-                    Occputaional Therapy
-                  </MenuItem>
-                  <MenuItem value={"Speech Therapy"}>Speech Therapy</MenuItem>
-                  <MenuItem value={"Aquatic Therapy"}>Aquatic Therapy</MenuItem>
-                  <MenuItem value={"ABA Therapy"}>ABA Therapy</MenuItem>
-                  <MenuItem value={"Massage Therapy"}>Massage Therapy</MenuItem>
-                </Select>
-              </FormControl>
-              <FormControl required fullWidth>
-                <InputLabel id="insurance">Insurance</InputLabel>
-
-                <Select
-                  labelId="insurance"
-                  id="insurance"
-                  value={insuranceValue}
-                  label="Insurance"
-                  onChange={handleInsuranceChange}
-                >
-                  <MenuItem value={"1199"}>1199</MenuItem>
-                  <MenuItem value={"AARP"}>AARP</MenuItem>
-                  <MenuItem value={"Aetna"}>Aetna</MenuItem>
-                  <MenuItem value={"Cigna"}>Cigna</MenuItem>
-                  <MenuItem value={"Empire BC/BS"}>Empire BC/BS</MenuItem>
-                  <MenuItem value={"Healthnet"}>Healthnet</MenuItem>
-                  <MenuItem value={"Horizon BC/BS of NJ"}>
-                    Horizon BC/BS of NJ
-                  </MenuItem>
-                  <MenuItem value={"Medicare"}>Medicare</MenuItem>
-                  <MenuItem value={"MPN"}>MPN</MenuItem>
-                  <MenuItem value={"Multiplan"}>Multiplan</MenuItem>
-                  <MenuItem value={"No Fault"}>No Fault</MenuItem>
-                  <MenuItem value={"Oxford"}>Oxford</MenuItem>
-                  <MenuItem value={"PHCS"}>PHCS</MenuItem>
-                  <MenuItem value={"Railroad Medicare"}>
-                    Railroad Medicare
-                  </MenuItem>
-                  <MenuItem value={"Secure Horizons"}>Secure Horizons</MenuItem>
-                  <MenuItem value={"Self Pay"}>Self Pay</MenuItem>
-                  <MenuItem value={"Tricare"}>Tricare</MenuItem>
-                  <MenuItem value={"United Health Care"}>
-                    United Health Care
-                  </MenuItem>
-                  <MenuItem value={"United Healthcare PO Box 740800"}>
-                    United Healthcare PO Box 740800
-                  </MenuItem>
-                  <MenuItem value={"Workers Compensation"}>
-                    Workers Compensation
-                  </MenuItem>
-                  <MenuItem value={"Other Insurance"}>Other Insurance</MenuItem>
-                  <MenuItem value={"No Insurance"}>No Insurance</MenuItem>
-                </Select>
-              </FormControl>
-              <TextField
-                required
-                multiline
-                rows={5}
-                id="message"
-                label="Message"
-                variant="outlined"
-              />
-              <Button type="submit" variant="outlined">
-                Submit
-              </Button>
-            </Stack>
-          </Paper>
-        </Box>
+                  <Select
+                    labelId="insurance"
+                    id="insurance"
+                    value={insuranceValue}
+                    label="Insurance"
+                    onChange={handleInsuranceChange}
+                  >
+                    <MenuItem value={"1199"}>1199</MenuItem>
+                    <MenuItem value={"AARP"}>AARP</MenuItem>
+                    <MenuItem value={"Aetna"}>Aetna</MenuItem>
+                    <MenuItem value={"Cigna"}>Cigna</MenuItem>
+                    <MenuItem value={"Empire BC/BS"}>Empire BC/BS</MenuItem>
+                    <MenuItem value={"Healthnet"}>Healthnet</MenuItem>
+                    <MenuItem value={"Horizon BC/BS of NJ"}>
+                      Horizon BC/BS of NJ
+                    </MenuItem>
+                    <MenuItem value={"Medicare"}>Medicare</MenuItem>
+                    <MenuItem value={"MPN"}>MPN</MenuItem>
+                    <MenuItem value={"Multiplan"}>Multiplan</MenuItem>
+                    <MenuItem value={"No Fault"}>No Fault</MenuItem>
+                    <MenuItem value={"Oxford"}>Oxford</MenuItem>
+                    <MenuItem value={"PHCS"}>PHCS</MenuItem>
+                    <MenuItem value={"Railroad Medicare"}>
+                      Railroad Medicare
+                    </MenuItem>
+                    <MenuItem value={"Secure Horizons"}>
+                      Secure Horizons
+                    </MenuItem>
+                    <MenuItem value={"Self Pay"}>Self Pay</MenuItem>
+                    <MenuItem value={"Tricare"}>Tricare</MenuItem>
+                    <MenuItem value={"United Health Care"}>
+                      United Health Care
+                    </MenuItem>
+                    <MenuItem value={"United Healthcare PO Box 740800"}>
+                      United Healthcare PO Box 740800
+                    </MenuItem>
+                    <MenuItem value={"Workers Compensation"}>
+                      Workers Compensation
+                    </MenuItem>
+                    <MenuItem value={"Other Insurance"}>
+                      Other Insurance
+                    </MenuItem>
+                    <MenuItem value={"No Insurance"}>No Insurance</MenuItem>
+                  </Select>
+                </FormControl>
+                <TextField
+                  required
+                  multiline
+                  rows={5}
+                  id="message"
+                  label="Message"
+                  variant="outlined"
+                />
+                <Button type="submit" variant="outlined">
+                  Submit
+                </Button>
+              </Stack>
+            </Paper>
+          </Box>
+        </form>
       </Zoom>
     </>
   );
--- components/contactus/ContactForm.js
+++ components/contactus/ContactForm.js
@@ -8,74 +8,113 @@ import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
 import Zoom from "@mui/material/Zoom";
 import { MuiTelInput } from "mui-tel-input";
+import emailjs from "@emailjs/browser";
+import { SuccessAlert, FailureAlert } from "../Alerts";
 
 export default function ContactForm(props) {
+  const [successOpen, setSuccessOpen] = React.useState(false);
+  const [failureOpen, setFailureOpen] = React.useState(false);
+
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setSuccessOpen(false);
+    setFailureOpen(false);
+  };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target.number.value);
+    let template = {
+      name: e.target.name.value,
+      email: e.target.email.value,
+      number: e.target.number.value,
+      message: e.target.message.value,
+    };
+
+    emailjs
+      .send(
+        "service_ulbsev4",
+        "template_bwljoma",
+        template,
+        "JjtNNxBts-8mfP1NA"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setSuccessOpen(true);
+        },
+        (error) => {
+          console.log(error.text);
+          setFailureOpen(true);
+        }
+      );
   };
   const [phone, setPhone] = React.useState("");
   const handleChange = (newValue) => {
     console.log(newValue);
     setPhone(newValue);
   };
+
   return (
     <>
+      <SuccessAlert open={successOpen} handleClose={handleClose} />
+      <FailureAlert open={failureOpen} handleClose={handleClose} />
       <Zoom in={true} style={{ transitionDelay: "250ms" }}>
-        <Box
-          component="form"
-          sx={{
-            "& > :not(style)": { m: 6, width: "55ch" },
-          }}
-          noValidate
-          autoComplete="off"
-          md={3}
-          display="flex"
-          justifyContent="center"
-          alignItems="center"
-          onSubmit={(e) => handleSubmit(e)}
-        >
-          <Paper
-            sx={{ m: 2, backgroundColor: "#9dc53f", borderRadius: "16px" }}
-            elevation={3}
+        <form onSubmit={(e) => handleSubmit(e)}>
+          <Box
+            sx={{
+              "& > :not(style)": { m: 6, width: "55ch" },
+            }}
+            noValidate
+            autoComplete="off"
+            md={3}
+            display="flex"
+            justifyContent="center"
+            alignItems="center"
           >
-            <Stack
-              sx={{ m: 5 }}
-              direction={"column"}
-              spacing={2}
-              textAlign="center"
+            <Paper
+              sx={{ m: 2, backgroundColor: "#9dc53f", borderRadius: "16px" }}
+              elevation={3}
             >
-              <Typography variant={"h5"}>Contact Form</Typography>
-              <TextField required id="name" label="Name" variant="outlined" />
-              <TextField
-                type="email"
-                required
-                id="email"
-                label="Email"
-                variant="outlined"
-              />
-              <MuiTelInput
-                value={phone}
-                onChange={handleChange}
-                required
-                id="number"
-                label="Number"
-                variant="outlined"
-              />
-              <TextField
-                required
-                multiline
-                rows={5}
-                id="message"
-                label="Message"
-                variant="outlined"
-              />
-              <Button type="submit" variant="outlined">
-                Submit
-              </Button>
-            </Stack>
-          </Paper>
-        </Box>
+              <Stack
+                sx={{ m: 5 }}
+                direction={"column"}
+                spacing={2}
+                textAlign="center"
+              >
+                <Typography variant={"h5"}>Contact Form</Typography>
+                <TextField required id="name" label="Name" variant="outlined" />
+                <TextField
+                  type="email"
+                  required
+                  id="email"
+                  label="Email"
+                  variant="outlined"
+                />
+                <MuiTelInput
+                  value={phone}
+                  onChange={handleChange}
+                  required
+                  id="number"
+                  label="Number"
+                  variant="outlined"
+                />
+                <TextField
+                  required
+                  multiline
+                  rows={5}
+                  id="message"
+                  label="Message"
+                  variant="outlined"
+                />
+                <Button type="submit" variant="outlined">
+                  Submit
+                </Button>
+              </Stack>
+            </Paper>
+          </Box>
+        </form>
       </Zoom>
     </>
   );
